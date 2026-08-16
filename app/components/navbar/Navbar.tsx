4fa0532f9e90809200aee018006c5b14a95c0cfb
natsu0001 <talukdarabhishek687@gmail.com>
2026-08-16 13:50:34 +0530

    forma dummy operation navfirst

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div>
+        <h1>GitScope</h1>
+
+        <div>
+          <a href="#">Dashboard</a>
+          <a href="#">Repositories</a>
+          <a href="#">Analytics</a>
+        </div>
+
+        <button>GitHub</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
