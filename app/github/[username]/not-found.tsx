a2d807a8a84994097beed0c5aad91048f440b545
natsu0001 <talukdarabhishek687@gmail.com>
2026-08-18 16:10:09 +0530

    usernot found

---
+++ app/github/[username]/not-found.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+import ErrorState from "@/app/components/ui/ErrorState";
+
+const NotFound = () => {
+  return (
+    <ErrorState
+      title="User not found"
+      message="We couldn't find that GitHub profile. Check the username and try again."
+    />
+  );
+};
+
+export default NotFound;
